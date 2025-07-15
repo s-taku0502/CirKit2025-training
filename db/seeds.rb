da88b8c98269cd2988c728546141b30f9376bd6e
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-07-15 20:36:45 +0900

    finish/controller-page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Book.create!(
+  name: "Project KIT vol.1",
+  price: 0,
+  publish_date: Date.new(2005, 5, 2),
+  comment: "先輩たちの残したプロジェクトKITの記録です。",
+	)
